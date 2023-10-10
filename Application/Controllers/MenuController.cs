da369c24f3384cc35129a5f6d60d392cac47e77f
Sunil Singh <er.techworld@gmail.com>
2023-10-10 22:10:51 +0530

    menus from api implemented

---
+++ Application/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMenus()
+        {
+            var menus = new List<string>()
+            {
+                "App Engineering/Sustaining",
+                "Progress & Forecasting Tool",
+                "Cook Learn - Q4 2020 Quarterly Release - (TID:N/A)"
+            };
+            return Ok(menus);
+        }
+    }
+}
